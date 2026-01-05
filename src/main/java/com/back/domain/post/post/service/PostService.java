854afab0d49cabe8dfd0a9c1a9f00b957d35a013
Rho <tmddjr_0331@naver.com>
2026-01-05 17:19:10 +0900

    0011

--- src/main/java/com/back/domain/post/post/service/PostService.java
+++ src/main/java/com/back/domain/post/post/service/PostService.java
@@ -1,5 +1,6 @@
 package com.back.domain.post.post.service;
 
+import com.back.domain.post.post.document.Post;
 import com.back.domain.post.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,7 +9,13 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class PostService {
     private final PostRepository postRepository;
+
     public long count() {
         return postRepository.count();
+    }
+
+    public Post create(String title, String content, String author) {
+        Post post = new Post(title, content, author);
+        return postRepository.save(post);
     }
 }
